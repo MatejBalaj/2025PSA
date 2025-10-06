57a08b4c8b17aeb02e1bd0d2eee36bb91fb38ff5
MatejBalaj <balaj5@tsud.uniza.sk>
2025-10-06 18:56:29 +0200

    autoCommitDoGitu

---
+++ git.py
@@ -0,0 +1,11 @@
+import os
+#repo_path = "c:\\Users\\matej\Desktop\\2025PSA"
+repo_path = r"c:\Users\matej\Desktop\2025PSA"
+repo_name = "2025PSA"
+repo_url =  "https://github.com/MatejBalaj/2025PSA.git"
+
+def clone_repo(path, url):
+    os.chdir(path)
+    os.system("git clone "+ url)
+
+clone_repo(repo_path, repo_url)
